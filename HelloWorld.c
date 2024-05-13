947a055f5a66c0d031cbfe1c109eb772e754b6e6
Shaurya Chundawat <chundawatshaurya@gmail.com>
2024-05-13 20:44:39 +0100

    Learnt Data Types is C

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main(){
     printf("Hello World");
+
+    int x; // declaration - allocates space in memory for variable
+    x = 123; // initialisation
+    int y = 321; // declaration and initialisation
+
+    char grade = 'A'; // single character
+    char name[] = "Shachu"; // array of characters or string    
+
     return 0;
 }
